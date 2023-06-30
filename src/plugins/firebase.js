cf31bfa668b93c477225fae7b0a58162dee5b2da
Hexagon-Dev <hexagon.dev.ua@gmail.com>
2023-06-30 21:46:41 +0300

    Set variables directly in code. Bad practice, but need to make it work.

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,13 +3,12 @@ import { getFirestore, collection } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseApp = initializeApp({
-  apiKey: import.meta.env.VITE_API_KEY ?? process.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN ?? process.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID ?? process.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET ?? process.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID
-    ?? process.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID ?? process.env.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY ?? 'AIzaSyDrZhGi-R_7CtRaCM3mRzRMgbJP-rZ70us',
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN ?? 'hexblogposter.firebaseapp.com',
+  projectId: import.meta.env.VITE_PROJECT_ID ?? 'hexblogposter',
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET ?? 'hexblogposter.appspot.com',
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID ?? '479227372846',
+  appId: import.meta.env.VITE_APP_ID ?? '1:479227372846:web:d11889a237dec19cf606bf',
 });
 
 const firebaseDB = getFirestore(firebaseApp);
